fa2628af64bfb9786d8c7d466ac1856e73316cb6
stauffenger <lucas.miranda96@hotmail.com>
2020-11-14 07:09:09 -0300

    Formatting the author and title on model.

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 class Book:
     def __init__(self, title, author, description):
-        self.title = title.upper()
-        self.author = author.upper()
+        self.title = title.replace("-", " ").upper()
+        self.author = author.replace("-", " ").upper()
         self.description = description
